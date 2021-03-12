e77013b1ff7b93085675569d41fb1e11843463cd
Yarosalv Adashian <Adashyan94@ukr.net>
2021-03-12 17:44:41 +0200

    Решил первых 5 задач

---
+++ lesson_1/hw_2.py
@@ -0,0 +1,40 @@
+# ex_1
+# is_next = None
+# num = int(input("Введите количество баллов: "))
+# if num >= 83:
+#     is_next = True
+
+# ex_2
+# is_admin = False
+# is_user = True
+# is_permission = True
+# access = None
+# if is_admin or is_permission:
+#     access = True
+# else:
+#     access = False
+
+# ex_3
+# work_experience = 4
+#
+# if 1 < work_experience <= 5:
+#     developer_type = "Middle"
+# elif work_experience <= 1:
+#     developer_type = "Junior"
+# else:
+#     developer_type = "Senior"
+
+# ex_4
+# num = 99
+#
+# if 0 < num:
+#     if num % 2 == 1:
+#         result = "Положительное нечетное число"
+#     else:
+#         result = "Положительное четное число"
+# elif 0 > num:
+#     result = "Отрицательное число"
+# else:
+#     result = "Это ноль"
+
+# ex_6
