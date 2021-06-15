34ea84faaf7e1dbf73e2e792021e68a88374c496
Yarosalv <79707144+Adashian@users.noreply.github.com>
2021-06-15 21:27:04 +0300

    Delete test.py

--- module_3/test.py
+++
@@ -1,9 +0,0 @@
-def get_fullname(first_name, last_name, middle_name=''):
-    x = ''
-    if middle_name:
-        x = first_name + ' ' + middle_name + ' ' + last_name
-        return x
-    x = first_name + ' ' + last_name
-    return x
-
-print(get_fullname('Anton', 'Kalash'))
